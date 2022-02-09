8ca597eb7297b0ba88cb990847d978219f2cff20
jishan <62906638+HasnathJami@users.noreply.github.com>
2022-02-10 01:57:57 +0600

    File uploaded

---
+++ app/src/main/java/com/example/apicallwithvolley/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.apicallwithvolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.getDataButtonId);
+        tv=findViewById(R.id.textId);
+
+       Volley volley;
+        RequestQueue requestQueue=Volley.newRequestQueue(this);
+        //String url="https://jsonplaceholder.typicode.com/todos/1";
+        String url="https://jsonplaceholder.typicode.com/posts";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+
+                        try {
+                            //String obj=response.getString("userId");
+
+                            //String ob=response.getString("userId");
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                            String str="";
+                            for(int i=0;i<response.length();i++)
+
+                            {
+                                JSONObject obj=response.getJSONObject(i);
+                                str=str + obj.getString("userId")+"\n";
+
+                            }
+                            tv.setText(str);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+
+                requestQueue.add(jsonArrayRequest);
+            }
+        });
+
+
+    }
+}
