ee59e256847bc3bf50b8a722f228a6558d7b1a96
jishan <62906638+HasnathJami@users.noreply.github.com>
2022-02-10 02:12:55 +0600

    File uploaded

--- app/src/main/java/com/example/apicallwithvolley/MainActivity.java
+++ app/src/main/java/com/example/apicallwithvolley/MainActivity.java
@@ -9,12 +9,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import com.android.volley.Request;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
